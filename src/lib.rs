7d6ff7e6b42658e4a93688eb712cf1739b6edd39
Rusty Brooks <rbrooks@Rustys-MacBook-Pro.local>
2024-02-24 12:49:26 -0600

    get build working properly

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,14 @@ use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn process()  {
+
+    let _ = processor::process();
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn event_correlator(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(process, m)?)?;
     Ok(())
 }
--- src/processor.rs
+++ src/processor.rs
@@ -1,6 +1,7 @@
+
 use std::io;
 
-fn process() -> io::Result<()> {
+pub fn process() -> io::Result<()> {
     let mut buffer = String::new();
     loop {
         io::stdin().read_line(&mut buffer)?;
